70562ce190290058d89a4a71ff048a086447fc24
solomon-prabu <solomonprabu002@gmail.com>
2022-08-23 21:45:22 +0530

    sample

---
+++ app.js
@@ -0,0 +1,9 @@
+const bodyBG = document.getElementById("body");
+const btnBG = document.getElementById("bgBtn");
+
+
+
+btnBG.addEventListener("click", function(){
+    bodyBG.style.background = "blue"
+})
+
